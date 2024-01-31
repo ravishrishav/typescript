d6ea47fba75dc824fbb80f36e5488b2e9e69a4c9
ravishrishav <ravishkumar.rishav@hcl.com>
2024-01-31 16:58:12 +0530

    2nd

---
+++ 01intro/intro.ts
@@ -0,0 +1,25 @@
+let myuser = {name: "hitesh", age: 10}
+
+console.log("Hitesh");
+//console.log(user.email);
+
+
+
+
+
+// tsc --init
+
+// tconfig
+
+// npm init -y
+
+// package.json
+
+// tsc -w
+
+// watchmode transpile
+
+// ouput dir tsconfig
+
+// npm i lite-server     package.json  start: "lite-server'
+
